4e0f7a57ebeecf8b41280a5057567ba6edd07975
Khan <realfriend7783@gmail.com>
2025-12-28 14:00:47 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/khan3.0/', // This must match your repo name exactly
 })
